e36254e355cd9842b146ab59653a76edd3b4ae0e
Dmitry Morozov <9984398@gmail.com>
2023-01-15 10:16:24 +0300

    Initial commit Pixel Class : Done

---
+++ uiuc/HSLAPixel.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file HSLAPixel.cpp
+ * Implementation of the HSLAPixel class for use in with the PNG library.
+ *
+ * @author University of Illinois CS 225 Course Staff
+ * @version 2018r1-lab1 - Updated for CS 400
+ */
+
+#include <cmath>
+#include <iostream>
+#include "HSLAPixel.h"
+using namespace std;
+
+namespace uiuc {
+    HSLAPixel::HSLAPixel() {
+        h = 0;
+        s = 0;
+        l = 1;
+        a = 1;
+    }
+
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = 1;
+    }
+
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance, double alpha) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = alpha;
+    }
+
+    bool HSLAPixel::operator==(const HSLAPixel & other) const {
+        return (h == other.h && s == other.s && l == other.l && a == other.a);
+    }
+
+    bool HSLAPixel::operator!=(const HSLAPixel & other) const {
+        return !(*this == other);
+    }
+}
